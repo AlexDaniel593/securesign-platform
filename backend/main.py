b82ec80cab2009e057edab5456414b57979d0664
AlexDaniel593 <dalexis203@gmail.com>
2026-07-29 20:51:58 -0500

    feat: update CORS settings and pass NEXT_PUBLIC_API_URL to frontend Docker build

--- backend/main.py
+++ backend/main.py
@@ -26,7 +26,12 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+        "http://192.168.56.101:3000",
+        "http://192.168.56.101",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
